bf7254c2b98962c954e559841728e5eca90fb7ce
fluffy <fluffy@fluffysnowman.dev>
2025-08-05 06:55:34 +0530

    update to v3 (taken format from `elog`) along with docs

--- README.md
+++ README.md
@@ -2,112 +2,177 @@
 
 PL is a simple, easy to use pretty logger made in go.
 
+> [!NOTE]  
+> The latest version produces the same logging output as
+> [elog](https://github.com/FluffySnowman/elog) along with the same available
+> config/string options.
+
+
 <!--toc:start-->
-- [Installation](#installation)
-- [Usage](#usage)
-- [All Logging Functions](#all-logging-functions)
+- [PL - Simple Pretty Logger](#pl-simple-pretty-logger)
+  - [Installation](#installation)
+  - [Usage (latest)](#usage-latest)
+    - [All available log types/methods/funcs](#all-available-log-typesmethodsfuncs)
+  - [Usage (old)](#usage-old)
+  - [All Logging Functions](#all-logging-functions)
 <!--toc:end-->
 
 
 ## Installation
 
-Use `go get` to install with the latest tag `v0.1.2` (recommended)
+Below are the instructions for `v0.1.2` and `v0.1.3`. `v0.1.3` is recommended.
 
-```bash
-go get -u github.com/fluffysnowman/prettylogger@v0.1.2
-```
+<details open>
+  <summary>Latest V3 Documentation</summary>
+  Use `go get` to install with the latest tag `v0.1.3` (recommended)
 
-or
+  ```bash
+  go get -u github.com/fluffysnowman/prettylogger@v0.1.3
+  ```
 
-Go's module suppert automatically fetches all the dependencies needed when you
-import it in your code so `go get` isn't required, however for this project, the
-`go get` installation is recommended with a specified tag (see the code block
-above for instructions).
+  Import: 
 
-```go
-// Importing and setting an alias to `pl` 
-import pl "github.com/fluffysnowman/prettylogger"
-```
+  ```go
+  import pl "github.com/fluffysnowman/prettylogger"
+  ```
+
+  ## Usage (latest)
+
+  Specify options on init seperated by commas. 
+
+  All available config/string options (taken from ./pl.go package doc comment): 
+
+  - TEA           : british time format (dd-mm-yy hh:mm:ss.mmm)
+  - BURGER        : american time format (mm-dd-yy hh:mm:ss.mmm)
+  - READABLE      : readable time format (ddmonyyyy hh:mm ss.mmm s)
+  - FILECOLOR     : write colour logs to file
+  - FILEONLY      : write logs only to file, no stdout output etc.  ..
+  - FILE=<path>   : write logs to specified file path, if exists, if not- created
 
 
-## Usage
+  Example usage:
 
-Initialise the pretty logger with a config (can be SIMPLE, SIMPLE2 or TIMEBASED)
-which is automatically used everywhere once set.
+  ```go 
+  pl.InitPrettyLogger("V3")                                       // original (just times, no dates) time format
+  pl.InitPrettyLogger("V3,TEA")                                   // TEA (bri'ish) time format
+  pl.InitPrettyLogger("V3,BURGER,FILECOLOR")                      // BURGER (american) time + colored file
+  pl.InitPrettyLogger("V3,READABLE,FILEONLY,FILE=logfile.log")    // readable time w/ month names etc, file-only written to logfile.log
+  ```
 
-Basic example:
+  ### All available log types/methods/funcs
 
-```go 
-package main
+  ```go
+  pl.Log("info message")
+  pl.LogDebug("debug message")
+  pl.LogWarn("warning message")
+  pl.LogError("error message")
+  ```
 
-import (
-  pl "github.com/fluffysnowman/prettylogger"
-)
+</details>
 
-func main() {
-  pl.InitPrettyLogger("SIMPLE")       // basic 
-  pl.LogInfo("Hello World")
-}
-```
+<details>
+  <summary>Older documentation (copy pasted, not updated)</summary>
+  ## Installation
 
-Using timestamps:
+  Use `go get` to install with the latest tag `v0.1.2` (recommended)
 
-```go 
-package main
+  ```bash
+  go get -u github.com/fluffysnowman/prettylogger@v0.1.2
+  ```
 
-import (
-  pl "github.com/fluffysnowman/prettylogger"
-)
+  or
 
-func main() {
-  pl.InitPrettyLogger("TIMEBASED")    // shows timestamps
-  pl.Log("Hello World")
-}
-```
+  Go's module suppert automatically fetches all the dependencies needed when you
+  import it in your code so `go get` isn't required, however for this project, the
+  `go get` installation is recommended with a specified tag (see the code block
+  above for instructions).
 
-Using the new `SIMPLE2` format:
+  ```go
+  // Importing and setting an alias to `pl` 
+  import pl "github.com/fluffysnowman/prettylogger"
+  ```
 
-```go
-package main
 
-import (
-  pl "github.com/fluffysnowman/prettylogger"
-)
+  ## Usage (old)
 
-func main() {
-  pl.InitPrettyLogger("SIMPLE2")    // detailed and concise format (new)
-  pl.Log("Hello World")
-}
-```
+  Initialise the pretty logger with a config (can be SIMPLE, SIMPLE2 or TIMEBASED)
+  which is automatically used everywhere once set.
 
-> [!IMPORTANT]  
-> Note that `SIMPLE`, `SIMPLE2` and `TIMEBASED` cannot be used together.
-> Changing or setting the InitPrettyLogger() multiple times may cause problems.
+  Basic example:
 
-Multiple arguments:
+  ```go 
+  package main
 
-```go
-pl.LogDebug("this is a debug log %v", "foo bar")
-```
+  import (
+    pl "github.com/fluffysnowman/prettylogger"
+  )
 
-## All Logging Functions
+  func main() {
+    pl.InitPrettyLogger("SIMPLE")       // basic 
+    pl.LogInfo("Hello World")
+  }
+  ```
 
-Below is a list of all the available functions.
+  Using timestamps:
 
-```go
-pl.InitPrettyLogger(opts)  // Accepts "SIMPLE", "SIMPLE2" or "TIMEBASED"
+  ```go 
+  package main
 
-pl.Log()          // green
-pl.LogDebug()     // cyan 
-pl.LogError()     // red
-pl.LogInfo()      // cyan
-pl.LogWarn()      // yellow
-pl.LogFatal()     // red
-pl.LogSuccess()   // green
-pl.LogFailure()   // yellow
-pl.LogOK()        // green
-pl.LogErrorBG()   // red background, white text
-pl.LogFailureBG() // yellow background, white text
-```
+  import (
+    pl "github.com/fluffysnowman/prettylogger"
+  )
+
+  func main() {
+    pl.InitPrettyLogger("TIMEBASED")    // shows timestamps
+    pl.Log("Hello World")
+  }
+  ```
+
+  Using the new `SIMPLE2` format:
+
+  ```go
+  package main
+
+  import (
+    pl "github.com/fluffysnowman/prettylogger"
+  )
+
+  func main() {
+    pl.InitPrettyLogger("SIMPLE2")    // detailed and concise format (new)
+    pl.Log("Hello World")
+  }
+  ```
+
+  > [!IMPORTANT]  
+  > Note that `SIMPLE`, `SIMPLE2` and `TIMEBASED` cannot be used together.
+  > Changing or setting the InitPrettyLogger() multiple times may cause problems.
+
+  Multiple arguments:
+
+  ```go
+  pl.LogDebug("this is a debug log %v", "foo bar")
+  ```
+
+  ## All Logging Functions
+
+  Below is a list of all the available functions.
+
+  ```go
+  pl.InitPrettyLogger(opts)  // Accepts "SIMPLE", "SIMPLE2" or "TIMEBASED"
+
+  pl.Log()          // green
+  pl.LogDebug()     // cyan 
+  pl.LogError()     // red
+  pl.LogInfo()      // cyan
+  pl.LogWarn()      // yellow
+  pl.LogFatal()     // red
+  pl.LogSuccess()   // green
+  pl.LogFailure()   // yellow
+  pl.LogOK()        // green
+  pl.LogErrorBG()   // red background, white text
+  pl.LogFailureBG() // yellow background, white text
+  ```
+</details>
+
 
 
--- pl.go
+++ pl.go
@@ -6,36 +6,47 @@ simplicity.
 
 # Usage
 
-Initialise the pretty logger with a config (can be SIMPLE, SIMPLE2 [newly added
-log format] or TIMEBASED) which is automatically used everywhere once set.
+Initialise the pretty logger with a config/string specifying options seperated
+by commas (,)
+
+Import alias for simplicity must be:
+
+	import (
+	    pl "github.com/fluffysnowman/prettylogger"
+	)
 
 Example:
 
-	pl.InitPrettyLogger("SIMPLE")       // basic
-	pl.InitPrettyLogger("SIMPLE2")      // new concise format
-	pl.InitPrettyLogger("TIMEBASED")    // shows timestamps
-	pl.LogInfo("Hello World")
+	pl.InitPrettyLogger("V3")                                       // original (just times, no dates) time format
+	pl.InitPrettyLogger("V3,TEA")                                   // TEA (bri'ish) time format
+	pl.InitPrettyLogger("V3,BURGER,FILECOLOR")                      // BURGER (american) time + colored file
+	pl.InitPrettyLogger("V3,READABLE,FILEONLY,FILE=logfile.log")    // readable time w/ month names etc, file-only written to logfile.log
+
+# All available config/string options
+
+  - TEA           : british time format (dd-mm-yy hh:mm:ss.mmm)
+  - BURGER        : american time format (mm-dd-yy hh:mm:ss.mmm)
+  - READABLE      : readable time format (ddmonyyyy hh:mm ss.mmm s)
+  - FILECOLOR     : write colour logs to file
+  - FILEONLY      : write logs only to file, no stdout output etc.  ..
+  - FILE=<path>   : write logs to specified file path, if exists, if not- created
 
 Multiple arguments:
 
-	pl.LogDebug("this is a debug log %v", "foo bar")
+	pl.LogDebug("this is a debug log %v", "meow meow mf")
 
-# All available log types
+# All available log types/methods/funcs
 
-	pl.Log("log")
-	pl.LogDebug("debug")
-	pl.LogError("error")
-	pl.LogInfo("info")
-	pl.LogFatal("fatal")
-	pl.LogSuccess("success")
-	pl.LogFailure("failure")
-	pl.LogOK("ok")
-	pl.LogErrorBG("errorbg")
-	pl.LogFailureBG("failerbg")
+	pl.Log("info message")
+	pl.LogDebug("debug message")
+	pl.LogWarn("warning message")
+	pl.LogError("error message")
 
 Author: @FluffySnowman (GitHub)
 
 Source: https://github.com/FluffySnowman/prettylogger
+
+License: MIT License
 */
 package prettylogger
 
@@ -43,371 +54,150 @@ package prettylogger
 
 import (
 	"fmt"
-	"io"
+	// "io"
 	"os"
 	"strings"
 	"time"
 )
 
-// Foreground colours (only text)
 const (
-	// Normal forground colours
-	RedFgANSI     = "\033[31m"
-	GreenFgANSI   = "\033[32m"
-	YellowFgANSI  = "\033[33m"
-	BlueFgANSI    = "\033[34m"
-	MagentaFgANSI = "\033[35m"
-	CyanFgANSI    = "\033[36m"
-	WhiteFgANSI   = "\033[37m"
-	BlackFgANSI   = "\033[30m"
-
-	// Bright foreground colours
-	BrightBlackFgANSI   = "\033[90m"
-	BrightRedFgANSI     = "\033[91m"
-	BrightGreenFgANSI   = "\033[92m"
-	BrightYellowFgANSI  = "\033[93m"
-	BrightBlueFgANSI    = "\033[94m"
-	BrightMagentaFgANSI = "\033[95m"
-	BrightCyanFgANSI    = "\033[96m"
-	BrightWhiteFgANSI   = "\033[97m"
+	COLOR_DEBUG = "\x1b[36m"
+	COLOR_INFO  = "\x1b[32m"
+	COLOR_WARN  = "\x1b[33m"
+	COLOR_ERROR = "\x1b[31m"
+	COLOR_RESET = "\x1b[0m"
 )
 
-// Background Colours (background of the text & won't affect the text content)
-const (
-	// Normal background colours
-	RedBgANSI     = "\033[41m"
-	GreenBgANSI   = "\033[42m"
-	YellowBgANSI  = "\033[43m"
-	BlueBgANSI    = "\033[44m"
-	MagentaBgANSI = "\033[45m"
-	CyanBgANSI    = "\033[46m"
-	WhiteBgANSI   = "\033[47m"
-	BlackBgANSI   = "\033[40m"
-
-	// Bright background colours (might not be used since the bright ones are
-	// harder to read)
-	BrightBlackBgANSI   = "\033[100m"
-	BrightRedBgANSI     = "\033[101m"
-	BrightGreenBgANSI   = "\033[102m"
-	BrightYellowBgANSI  = "\033[103m"
-	BrightBlueBgANSI    = "\033[104m"
-	BrightMagentaBgANSI = "\033[105m"
-	BrightCyanBgANSI    = "\033[106m"
-	BrightWhiteBgANSI   = "\033[107m"
-)
-
-// All text formatting ansi codes
-const (
-	ResetANSI = "\033[0m"
-)
-
-// Struct for all the log types so that diff log formats can be defined without
-// repeating shit
-type LogTypes struct {
-	LogLog     string
-	DebugLog   string
-	ErrorLog   string
-	FatalLog   string
-	InfoLog    string
-	SuccessLog string
-	FailedLog  string
-	OkayLog    string
-	WarnLog    string
+var cfg struct {
+	timeMode  int
+	fileColor bool
+	fileOnly  bool
+	logFile   *os.File
 }
 
-// Format for normal SIMPLE logs (original)
-var SimpleLog = LogTypes{
-	LogLog:     "[%s   LOG   %s%s] %v\n",
-	DebugLog:   "[%s  DEBUG  %s%s] %v\n",
-	ErrorLog:   "[%s  ERROR  %s%s] %v\n",
-	FatalLog:   "[%s  FATAL  %s%s] %v\n",
-	InfoLog:    "[%s  INFO   %s%s] %v\n",
-	SuccessLog: "[%s SUCCESS %s%s] %v\n",
-	FailedLog:  "[%s FAILURE %s%s] %v\n",
-	OkayLog:    "[%s   OK    %s%s] %v\n",
-}
+/*
+OPTS: TEA, BURGER, READABLE, FILECOLOR, FILEONLY, FILE=<path>
 
-// Format for timestamp TIMEBASED logs (original)
-var TimestampLog = LogTypes{
-	LogLog:     "[%s   LOG   %s%s] %v\n",
-	DebugLog:   "[%s  DEBUG  %s%s] %v\n",
-	ErrorLog:   "[%s  ERROR  %s%s] %v\n",
-	FatalLog:   "[%s  FATAL  %s%s] %v\n",
-	InfoLog:    "[%s  INFO   %s%s] %v\n",
-	SuccessLog: "[%s SUCCESS %s%s] %v\n",
-	FailedLog:  "[%s FAILURE %s%s] %v\n",
-	OkayLog:    "[%s   OK    %s%s] %v\n",
-}
-
-// format for SIMPLE2 logs
-var Simple2Log = LogTypes{
-	LogLog:     "INFO [%s|%s] %v\n", // log is info in simple2
-	DebugLog:   "DEBUG[%s|%s] %v\n",
-	ErrorLog:   "ERROR[%s|%s] %v\n",
-	FatalLog:   "FATAL[%s|%s] %v\n",
-	InfoLog:    "INFO [%s|%s] %v\n",
-	SuccessLog: "INFO [%s|%s] %v\n", // success is info
-	FailedLog:  "ERROR[%s|%s] %v\n", // failure is error
-	OkayLog:    "INFO [%s|%s] %v\n", // ok is info
-	WarnLog:    "WARN [%s|%s] %v\n",
-}
-
-// Configuration for the logger
-type PrettyLogger struct {
-	writer   io.Writer
-	logLevel string
-	color    string
-	logType  string
-}
-
-func getCurrentTimestamp() string {
-	return time.Now().Format("2006/01/02 15:04:05")
-}
-
-func getSimple2Timestamp() string {
-	return time.Now().Format("01-02|15:04:05.000")
-	// return time.Now().Format("01-02-2006|15:04:05.000")
-}
-
-// Global pretty logger instance (used to r/w config from)
-var prettyLoggerConfig *PrettyLogger
-
-// sets the log format to SIMPLE, SIMPLE2 (newly added format) or TIMEBASED.
-//
-// Default is set to SIMPLE.
-//
-// Do not set the log format multiple times as it will cause problems
-func InitPrettyLogger(prettyLogType string) {
-	if len(prettyLogType) < 1 {
-		prettyLogType = "SIMPLE"
-	}
-	prettyLoggerConfig = &PrettyLogger{
-		writer:  os.Stdout,
-		color:   WhiteFgANSI,
-		logType: prettyLogType,
-	}
-}
-
-func getLogType() LogTypes {
-	switch prettyLoggerConfig.logType {
-	case "SIMPLE":
-		return SimpleLog
-	case "TIMEBASED":
-		return TimestampLog
-	case "SIMPLE2":
-		return Simple2Log
-	default:
-		return SimpleLog
-	}
-}
-
-func printLog(logFormat string, logColor string, message string, timestamp bool) {
-	if prettyLoggerConfig == nil {
-		return
-	}
-
-	if prettyLoggerConfig.logType == "SIMPLE2" {
-		timestampStr := getSimple2Timestamp()
-		parts := strings.SplitN(timestampStr, "|", 2)
-		datePart := parts[0]
-		timePart := parts[1]
-
-		levelWord := ""
-		restFormat := ""
-		{
-			idxBracket := strings.Index(logFormat, "[")
-			idxSpace := strings.Index(logFormat, " ")
-			if idxBracket == -1 {
-				idxBracket = len(logFormat)
+  - TEA           : british time format (dd-mm-yy hh:mm:ss.mmm)
+  - BURGER        : american time format (mm-dd-yy hh:mm:ss.mmm)
+  - READABLE      : readable time format (ddmonyyyy hh:mm ss.mmm s)
+  - FILECOLOR     : write colour logs to file
+  - FILEONLY      : write logs only to file, no stdout output etc.  ..
+  - FILE=<path>   : write logs to specified file path, if exists, if not- created
+*/
+func InitPrettyLogger(opts string) {
+	cfg = struct {
+		timeMode  int
+		fileColor bool
+		fileOnly  bool
+		logFile   *os.File
+	}{}
+	for _, o := range strings.Fields(strings.ReplaceAll(opts, ",", " ")) {
+		u := strings.ToUpper(o)
+		switch {
+		case u == "TEA":
+			cfg.timeMode = 1
+		case u == "BURGER":
+			cfg.timeMode = 2
+		case u == "READABLE":
+			cfg.timeMode = 3
+		case u == "FILECOLOR":
+			cfg.fileColor = true
+		case u == "FILEONLY":
+			cfg.fileOnly = true
+		case strings.HasPrefix(u, "FILE="):
+			path := o[len("FILE="):]
+			if f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644); err == nil {
+				cfg.logFile = f
 			}
-			if idxSpace == -1 {
-				idxSpace = len(logFormat)
-			}
-
-			endIdx := idxBracket
-			if idxSpace != -1 && idxSpace < idxBracket {
-				endIdx = idxSpace
-			}
-			levelWord = logFormat[:endIdx]
-			restFormat = logFormat[len(levelWord):]
 		}
-
-		coloredDate := YellowFgANSI + datePart + ResetANSI
-		coloredTime := CyanFgANSI + timePart + ResetANSI
-		coloredMessage := ResetANSI + message + ResetANSI
-
-		fmt.Fprintf(prettyLoggerConfig.writer,
-			"%s%s%s"+restFormat,
-			logColor, levelWord, ResetANSI,
-			coloredDate, coloredTime, coloredMessage,
-		)
-		return
 	}
-
-	timestampStr := ""
-	if timestamp || prettyLoggerConfig.logType == "TIMEBASED" {
-		timestampStr = " " + getCurrentTimestamp() + " "
-	}
-
-	fmt.Fprintf(
-		prettyLoggerConfig.writer,
-		logFormat,
-		logColor,
-		timestampStr,
-		ResetANSI,
-		message,
-	)
 }
 
-func Log(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.LogLog, GreenFgANSI, formattedMessage, false)
+func formattedTime() string {
+	now := time.Now()
+	ms := now.Nanosecond() / 1e6
+	switch cfg.timeMode {
+	// case 1, 2:
+    // british format
+    case 1:
+		// dd-mm-yy HH:MM:SS.mmm
+		return fmt.Sprintf("%02d-%02d-%02d %02d:%02d:%02d.%03d",
+			now.Day(), now.Month(), now.Year()%100,
+			now.Hour(), now.Minute(), now.Second(), ms)
+    // american format for 2 
+    case 2: 
+		// mm-dd-yy HH:MM:SS.mmm
+		return fmt.Sprintf("%02d-%02d-%02d %02d:%02d:%02d.%03d",
+			now.Month(), now.Day(), now.Year()%100,
+			now.Hour(), now.Minute(), now.Second(), ms)
+	case 3:
+		// DDMonYYYY HH:MM SS.mmm s
+		mon := []string{"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+		p1 := fmt.Sprintf("%02d%s%04d", now.Day(), mon[now.Month()-1], now.Year())
+		p2 := fmt.Sprintf("%02d:%02d", now.Hour(), now.Minute())
+		p3 := fmt.Sprintf("%02d.%03ds", now.Second(), ms)
+		return fmt.Sprintf("%s %s %s", p1, p2, p3)
+	default:
+		// HH:MM:SS.mmm
+		return fmt.Sprintf("%02d:%02d:%02d.%03d",
+			now.Hour(), now.Minute(), now.Second(), ms)
+	}
+}
+
+func logV3(levelTag, color, msg string) {
+	ts := formattedTime()
+	lineOut := fmt.Sprintf("%s %s%s%s %s\n", ts, color, levelTag, COLOR_RESET, msg)
+	if !cfg.fileOnly {
+		fmt.Fprint(os.Stdout, lineOut)
+	}
+	if f := cfg.logFile; f != nil {
+		if cfg.fileColor {
+			fmt.Fprint(f, lineOut)
+		} else {
+			fmt.Fprintf(f, "%s %s %s\n", ts, levelTag, msg)
+		}
+		_ = f.Sync()
+	}
 }
 
 func LogDebug(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.DebugLog, CyanFgANSI, formattedMessage, false)
+	logV3("DBG", COLOR_DEBUG, fmt.Sprintf(format, a...))
 }
 
-func LogError(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.ErrorLog, RedFgANSI, formattedMessage, false)
-}
-
-func LogInfo(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.InfoLog, GreenFgANSI, formattedMessage, false)
-}
-
-func LogFatal(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.FatalLog, BrightRedFgANSI, formattedMessage, false)
-}
-
-func LogSuccess(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.SuccessLog, GreenFgANSI, formattedMessage, false)
-}
-
-func LogFailure(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.FailedLog, RedFgANSI, formattedMessage, false)
-}
-
-func LogOK(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.OkayLog, GreenFgANSI, formattedMessage, false)
+func Log(format string, a ...interface{}) {
+	logV3("INF", COLOR_INFO, fmt.Sprintf(format, a...))
 }
 
 func LogWarn(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.WarnLog, YellowFgANSI, formattedMessage, false)
+	logV3("WRN", COLOR_WARN, fmt.Sprintf(format, a...))
 }
 
-func LogErrorBG(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.ErrorLog, RedBgANSI, formattedMessage, false)
+func LogError(format string, a ...interface{}) {
+	logV3("ERR", COLOR_ERROR, fmt.Sprintf(format, a...))
 }
 
-func LogFailureBG(format string, a ...interface{}) {
-	logFormats := getLogType()
-	formattedMessage := fmt.Sprintf(format, a...)
-	printLog(logFormats.FailedLog, YellowBgANSI, formattedMessage, false)
+func main() {
+
+	// // default shit test
+	// InitPrettyLogger("V3")
+	// Log("hello there with default shit")
+	// LogDebug("eeeeee")
+	// LogWarn("warning here")
+	// LogError("error shit: %v", "lol")
+
+	// diff modes test
+	InitPrettyLogger("V3,BURGER,FILECOLOR,FILE=app.log")
+	Log("hello there")
+	LogDebug("eeeeee")
+	LogWarn("warning here")
+	LogError("error shit: %v", "lol")
+
+	// InitPrettyLogger("V3,READABLE,FILEONLY,FILE=readable.log")
+	// Log("hello there")
+	// LogDebug("eeeeee")
+	// LogWarn("warning here")
+	// LogError("error shit: %v", "lol")
+
 }
-
-// // Using main here for testing
-// func main() {
-
-// 	// Init the logger with simple/complex config
-// 	// InitPrettyLogger("SIMPLE")
-// 	// InitPrettyLogger("TIMEBASED")
-// 	InitPrettyLogger("SIMPLE2")
-// 	// LogDebug("this is a DEBUG log").Print()
-// 	// LogError("this is a error log").Print()
-// 	// LogSuccess("this is a success log").Print()
-// 	// LogInfo("this is a INFO log").Print()
-// 	// LogOK("this is ok").Print();
-// 	// LogOK("this is ok").Print();
-// 	// LogOK("this is ok").Print();
-// 	// LogOK("this is ok").Print();
-// 	// LogOK("this is ok").Print();
-// 	// LogDebug("this i sa debeg log over here ").Print()
-// 	// println()
-// 	Log("log")
-// 	LogDebug("debug")
-//     fmt.Println("debug")
-// 	LogError("error")
-// 	LogInfo("info")
-// 	// LogWarn("warn")
-// 	LogFatal("fatal")
-// 	LogSuccess("success")
-// 	LogFailure("failure")
-// 	LogOK("ok")
-// 	LogErrorBG("errorbg")
-// 	LogFailureBG("failerbg")
-// 	// Log("connecting to database...")
-// 	// LogOK("database connected")
-// 	// LogSuccess("database connected")
-// 	// LogInfo("this should be an info log")
-// 	// LogInfo("this should be an info log")
-// 	// LogDebug("query: SELECT * FROM users WHERE username = $1")
-// 	// LogFailure("failed to execute query")
-// 	// LogFailureBG("failed to execute query")
-// 	// LogFatal("segmentation fault, core dumped")
-// 	// LogErrorBG("DUMPING CORE..")
-// 	//     LogInfo("preparing to execute query...").Print()
-// 	//     // LogError("DUMPING CORE..").Print()
-
-// 	// // println("testing all log types below to see how they look\n")
-// 	// // Log("hello there").Print();
-// 	// // LogDebug("hello there").Print();
-// 	// // LogError("hello there").Print();
-// 	// // LogInfo("hello there").Print();
-// 	// // LogFatal("hello there").Print();
-// 	// // LogSuccess("hello there").Print();
-// 	// // LogFailure("hello there").Print();
-// 	// // LogOK("hello there").Print();
-// }
-
-// 	LogDebug("this is a debug log %v", "which should print something").Timestamp().Print()
-// 	// LogDebug("this is a debug log %v", "which should print something").Timestamp().Print()
-// 	LogInfo("job info: %v ", "running job ...")
-// 	LogInfo("job info: %v ", "job SUCCESS").Timestamp().Print()
-
-// // Testing different data types to see if it works
-// var someString string
-// var someInt int
-// var someFloat float64
-// someString = "this is some string"
-// someInt = 42069
-// someFloat = 2981389.829810
-
-// LogError("some ERROR shit here")
-// LogDebug("this is a debug log")
-// LogInfo("some info here")
-// LogSuccess("success with some int -> %v", someInt)
-// LogFailure("this is a failure message")
-// LogFatal("failed to 420: %v %v", someString, someFloat)
-
-// Example of logging with timestamp
-// LogDebug("this is a debug log with timestamp").Timestamp().Print()
-// LogInfo("some info here with timestamp").Timestamp().Print()
-// }
-
-// Test with basic raw ascii for comparison
-// var errorTest error
-// errorTest = errors.New("some error happened")
-// errorTest := nil
-// if errorTest == nil {
-// 	fmt.Printf("\033[31m[ ERROR ] \033[97;40m%v\033[0m\n", errorTest)
-// }
